2cce40c0e8e51ee09de0b9df17a2ead6bcfea54a
Hasrat <hsabit111@gmail.com>
2017-07-16 13:32:41 -0400

    navbar is ready

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
